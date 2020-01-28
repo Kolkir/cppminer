0347a32234eb3f2653dffd93d3594248ee819636
Kolkir <rotate@ukr.net>
2020-01-28 08:40:33 +0200

    Parser implementation

---
+++ src/ast_utils.py
@@ -0,0 +1,13 @@
+from networkx import Graph
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+def ast_to_graph(ast):
+
--- src/astparser.py
+++ src/astparser.py
@@ -41,7 +41,7 @@ class AstParser:
 
     def __parse_function(self, func_node):
         key = self.__tokenize(func_node.spelling)
-        print("Function " + str(key))
+        print("Function " + str(key) + " " + func_node.spelling)
         terminal_nodes = self.__get_terminal_nodes(func_node)
         paths = self.__get_paths(func_node, terminal_nodes)
         sample = Sample(key, paths)
@@ -67,8 +67,17 @@ class AstParser:
         return tokens
 
     def __get_terminal_nodes(self, func_node):
-        # TODO: Implement
         terminal_nodes = []
+
+        def get_trem_nodes(nodes, out_nodes):
+            for n in nodes:
+                children = list(n.get_children())
+                if len(children) == 0:
+                    out_nodes.append(n)                    
+                else:
+                    get_trem_nodes(children, out_nodes)
+
+        get_trem_nodes(list(func_node.get_children()), terminal_nodes)
         return terminal_nodes
 
     def __get_paths(self, func_node, terminal_nodes):
